6c53282f9aee4db32975a759274d6055c203dda2
AsukaKanwmatsu <asuka.kanematsu@en-gei.co.jp>
2023-04-30 22:54:03 +0900

    [PHP基礎]関数　課題1-②

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,13 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+$spendings = ["apple" => 100, "orange" => 150];
+$incomes = ["okozukai" => 10000, "partTimeSalary" => 35000];
+echo findTotlIncomes($incomes);
+
+function findTotlIncomes($incomes)
+{
+    $totalIncomes = 0;
+    foreach ($incomes as $income) {
+        $totalIncomes += $income;
+    }
+    return $totalIncomes;
+}
